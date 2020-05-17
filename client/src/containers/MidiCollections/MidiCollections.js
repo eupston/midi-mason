7bca6922ccac790202e32ece5ba469f26b806bca
eupston <eupston130@hotmail.com>
2020-05-17 23:56:45 +1200

    updated with jukebox and 3D header

--- client/src/containers/MidiCollections/MidiCollections.js
+++ client/src/containers/MidiCollections/MidiCollections.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import MidiCollection from "../MidiCollection/MidiCollection";
+import JukeBox from "../JukeBox/Jukebox";
 
 const MidiCollections = () => {
     return (
         <React.Fragment >
-            <MidiCollection title={"All Beats"} filterParams={{limit: 50}}/>
-            <MidiCollection title={"Recently Made"} filterParams={{limit: 50}}/>
-            <MidiCollection title={"Electro"} filterParams={{limit: 50}}/>
+            {/*<MidiCollection title={"All Beats"} filterParams={{limit: 50}}/>*/}
+            <JukeBox filterParams={{limit: 50}} title={"All Beats"}/>
         </React.Fragment>
     );
 };
