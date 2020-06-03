cc0565246027f5dc3ffb1f10ddb5e98bef1ab5dd
eupston <eupston130@hotmail.com>
2020-06-03 22:46:28 +1200

    updated with latest client build

---
+++ build/precache-manifest.176419080dfb8c20bb45584c2b51cbf9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d8f7ab008ee982c4b98cd771efc1593",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6390f3cfbd3fc5fd149",
+    "url": "/static/css/2.1f610178.chunk.css"
+  },
+  {
+    "revision": "9083b42c0841c6022cbb",
+    "url": "/static/css/main.86fec4b0.chunk.css"
+  },
+  {
+    "revision": "e6390f3cfbd3fc5fd149",
+    "url": "/static/js/2.ef33fca6.chunk.js"
+  },
+  {
+    "revision": "fd1069225f978dd5a79acc810674e0a7",
+    "url": "/static/js/2.ef33fca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9083b42c0841c6022cbb",
+    "url": "/static/js/main.c33c3a89.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  },
+  {
+    "revision": "0e6ccc6b835ded486564fb8e789bf522",
+    "url": "/static/media/midi_mason_logo_white.0e6ccc6b.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
