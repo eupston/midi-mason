fdd8f583d4544e13766c42ca1c6230884d5b01bc
eupston <eupston130@hotmail.com>
2020-06-22 23:48:38 +1200

    updated with latest client build 2

---
+++ build/precache-manifest.cc89c3df0a66617e6ba3ff17f8bd9fd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af78d3a6c07f6ed839944be630ed72c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ceed3b35f3a7ad29035",
+    "url": "/static/css/main.1c9d267d.chunk.css"
+  },
+  {
+    "revision": "5809d3dd47401c430c47",
+    "url": "/static/js/2.bcdf5903.chunk.js"
+  },
+  {
+    "revision": "fd1069225f978dd5a79acc810674e0a7",
+    "url": "/static/js/2.bcdf5903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ceed3b35f3a7ad29035",
+    "url": "/static/js/main.a235132e.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  },
+  {
+    "revision": "0e6ccc6b835ded486564fb8e789bf522",
+    "url": "/static/media/midi_mason_logo_white.0e6ccc6b.png"
+  }
+]);
